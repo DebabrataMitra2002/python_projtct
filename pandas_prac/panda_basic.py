2956b8f141f82d50a16184724d6c756a80e66828
DebabrataMitra2002 <debabratajv2019@gmail.com>
2024-02-28 00:13:25 +0530

    PANDAS FILE ADD

---
+++ pandas_prac/panda_basic.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+
+# print(pd.__version__)
+
+
+# print(pd.__path__)
+
+
+# a=[12,34,45]
+# myVar=pd.Series(a)
+# print(myVar)
+
+
+# print(pd.Series(a,index=["a","b","c"]))
+
+# calories = {"day1": 420, "day2": 380, "day3": 390}
+
+# myvar = pd.Series(calories, index = ["day1", "day2"])
+
+# print(myvar)
+
+""" DataFrames
+ Data sets in Pandas are usually multi-dimensional tables, called DataFrames.
+
+ Series is like a column, a DataFrame is the whole table """
+
+# myDataset= {
+#     'name':["debo","dola","baban"],
+#     'marks':[12,23,34]
+# }
+# print(pd.DataFrame(myDataset))
+
