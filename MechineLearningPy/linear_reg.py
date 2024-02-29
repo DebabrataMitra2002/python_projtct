5a3272b1c23ab67d27162f9a15c1c56f962d286a
DebabrataMitra2002 <debabratajv2019@gmail.com>
2024-03-01 00:31:56 +0530

    new file

---
+++ MechineLearningPy/linear_reg.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn import linear_model as lm
+import matplotlib.pyplot as plt
+
+# Read the dataset
+df = pd.read_csv('homeprices.csv')
+
+# Plotting the scatter graph
+plt.xlabel('area (sqr ft)')
+plt.ylabel('prices (US$)')
+plt.scatter(df.area, df.price, color='red', marker='+')
+plt.show()
+
+# Create a linear regression model object
+reg = lm.LinearRegression()
+
+# Prepare the data
+X = df[['area']]  # Feature matrix
+y = df['price']   # Target variable
+
+# Train the model
+reg.fit(X, y)
+
+# Predict the price for a new area value of 3300 square feet
+predicted_price = reg.predict([[3300]])
+print("Predicted price for an area of 3300 sqft:", predicted_price)
--- MechineLearningPy/panda_basic.py
+++ MechineLearningPy/panda_basic.py
@@ -1,8 +1,12 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model as lm
 
 
 # print(pd.__version__)
+# print(plt.__version__)
+# print(np.__version__)
 
 
 # print(pd.__path__)
@@ -32,3 +36,17 @@ import pandas as pd
 # }
 # print(pd.DataFrame(myDataset))
 
+
+# Load data csv file to data frame:
+df = pd.read_csv('data.csv')
+print(df.to_string())
+print(df)
+df.plot(kind='scatter',x='Duration',y='Pulse')
+print(df.corr())
+plt.show()
+# print(df.info())
+ 
+# x=np.array([0,8])
+# y=np.array([0,8])
+# plt.plot(x,y,marker='h',color='red',ls=':')
+# plt.show()
